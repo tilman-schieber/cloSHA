e8c013cded01eab3189f9a2b46f0308da1561c82
Tilman <tilman.schieber@gmail.com>
2017-07-07 00:55:49 +0200

    added main function

--- src/closha/core.clj
+++ src/closha/core.clj
@@ -1,10 +1,17 @@
 (ns closha.core
     (:require [closha.helpers :refer :all]
               [closha.sha256 :refer :all]
-              [closha.aes :refer :all]))
+              [closha.aes :refer :all])
+    (:gen-class))
 
 ; CloSHA - SHA algorithms in clojure
 ;
 ; Reference:
 ; http://csrc.nist.gov/publications/fips/fips180-4/fips-180-4.pdf
 ;
+(defn -main
+  "Entry point"
+  [& args]
+  (println (if (empty? args)
+               "Please give a string to hash as a parameter"
+               (sha256 (first args)))))
