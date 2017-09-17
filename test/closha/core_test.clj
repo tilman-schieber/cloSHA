f8cc7c724accf424349ae6d84b9785e2f286b79a
Tilman <tilman.schieber@gmail.com>
2017-09-17 20:08:22 +0200

    changes test

--- test/closha/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns closha.core-test
-  (:require [clojure.test :refer :all]
-            [closha.core :refer :all]))
-
-(deftest inverse-test
-  (testing "test the multiplicative inverse elements for all x in GF(2^8)"
-    (is (every? (partial = 1) (map #(gmul % (ginv %)) (range 1 255))))))
